ef76a5f0a2cb2952a2588081ca1f158ffede61e2
D_Zucho <danny.nico.dev@outlook.com>
2022-08-31 17:36:49 -0700

    add sidebar components and style

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,29 @@
+// import { useState, useEffect } from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import SideBar from './SideBar'
+
 const Feed = () => {
   return (
-    <div>
-      <h1>Feed</h1>
-    </div>
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+      {/* This means that usually it is a column, but at MEDIUM DEVICES and up, 
+      it will be set to row */}
+      <Box
+        sx={{
+          height: { sx: 'auto', md: '92vh' },
+          borderRight: '1px solid #3d3d3d',
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar />
+        <Typography
+          className='copyright'
+          variant='body2'
+          sx={{ mt: 1.5, color: '#fff' }}
+        >
+          Copyright 2022 Zukko Media
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,28 @@
+import { Link } from 'react-router-dom'
+import { Stack } from '@mui/material'
+import { logo } from '../utilities/constants.js'
+import SearchBar from './SearchBar'
+
 const NavBar = () => {
   return (
-    <div>
-      <h1>nav</h1>
-    </div>
+    <Stack
+      direction='row'
+      align-items='center'
+      p={2}
+      // sx prop allows us to give it our own styles
+      sx={{
+        position: 'sticky',
+        background: '#000',
+        top: 0,
+        justifyContent: 'space-between',
+      }}
+      spacing={4}
+    >
+      <Link to='/' style={{ display: 'flex, alignItems: center' }}>
+        <img src={logo} alt='logo' height={45} />
+      </Link>
+      <SearchBar />
+    </Stack>
   )
 }
 
